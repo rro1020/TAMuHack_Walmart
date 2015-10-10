77cfe3a2d85f36bdde5507eb93230a94547643e0
Roel Orduno <rro1020@gmail.com>
2015-10-10 12:50:12 -0500

    Edited Files

--- js/hello.js
+++ js/hello.js
@@ -11,9 +11,35 @@ helloApp.controller("myCtrl",  function ($scope, $http) {
 	}; 
 	$http.defaults.useXDomain = true; 
 	delete $http.defaults.headers.common['X-Requested-With']; 
+
+	//This works with the Value of the Day API 
     $http.get('https://api.walmartlabs.com/v1/vod?format=json&apiKey=' + apiKey).
         success(function(data) {
-            $scope.greeting = data;
+            $scope.valueOfDay = data;
+
+            var data = {
+            	"images" : [{ "largeImg" : $scope.valueOfDay.largeImage }]
+            }; 
+            data.images.forEach(function(obj){
+            	var img = new Image(); 
+            	img.src = obj.largeImg; 
+            	img.setAttribute("class", "valueImage"); 
+            	document.getElementById("img-container").appendChild(img); 
+            });
             //console.log(greeting); 
-        });
-}); 
+        });	
+        
+     //This works with the Trending API  
+     $http.get('http://api.walmartlabs.com/v1/trends?format=json&apiKey=' + apiKey). 
+     	success(function(data){
+     		$scope.trending = data; 
+     		/*
+     		for (val = 1; val < $scope.trending.length; val++){
+
+     		}
+			*/
+     	}); 
+ 
+});
+
+
